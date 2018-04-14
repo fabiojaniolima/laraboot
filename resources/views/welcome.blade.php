13ae8bf0e3d9315ad7a1a518c7df67e4641ddaad
fabiojaniolima <fabiojaniolima@gmail.com>
2018-04-14 20:05:17 -0300

    Add lang pt-BR
    O suporte foi adicionado para as mensagens de callback (validação e outras).

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,8 +71,8 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a href="{{ route('register') }}">{{ __('Register') }}</a>
                     @endauth
                 </div>
             @endif
