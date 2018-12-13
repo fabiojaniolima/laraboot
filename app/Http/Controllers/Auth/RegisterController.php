4f6593637866504aea210511600622c882168353
fabiojaniolima <fabiojaniolima@gmail.com>
2018-12-13 11:33:11 -0200

    Nome tem que ser >= 3

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,7 +49,7 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'min:3', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:'.User::$min_password_size, 'confirmed'],
         ]);
