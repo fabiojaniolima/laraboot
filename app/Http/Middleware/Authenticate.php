7cd0e20c040df722447ccfdc79d390b008bf357d
fabiojaniolima <fabiojaniolima@gmail.com>
2018-12-13 16:12:11 -0200

    Corrigido método de autenticação, fixed #2

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Lang;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
@@ -26,6 +27,7 @@ class Authenticate extends Middleware
      *
      * @param \Illuminate\Http\Request $request
      * @param array $guards
+     * @throws AuthenticationException
      */
     protected function authenticate($request, array $guards)
     {
