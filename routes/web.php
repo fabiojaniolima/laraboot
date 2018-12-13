194424780f812145297b4bf7d8f083667d92d5c2
fabiojaniolima <fabiojaniolima@gmail.com>
2018-12-13 11:29:20 -0200

    Rota home removida

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,6 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::prefix('painel')->middleware('auth')->group(function () {
     Route::get('/', 'Panel\HomeController@index')->name('dashboard');
 
