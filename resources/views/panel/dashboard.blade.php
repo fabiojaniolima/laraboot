e962d1bb2f2d36d8b6be412a6d8c0415331486f1
fabiojaniolima <fabiojaniolima@gmail.com>
2019-02-13 18:04:57 -0200

    Add visão de total de usuários, bloqueados e ativos

--- resources/views/panel/dashboard.blade.php
+++ resources/views/panel/dashboard.blade.php
@@ -10,7 +10,54 @@
                 <div class="card-body">
                     @include('includes.alert')
 
-                    Login efetuado com sucesso!
+                    <h5>Usuários</h5>
+
+                    <hr>
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card bg-light mb-3">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h5 class="card-title">Total: {{ $total_users }}</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <i class="fas fa-users fa-2x text-dark"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card bg-light mb-3">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h5 class="card-title">Bloqueados: {{ $locked_users }}</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <i class="fas fa-user-times fa-2x text-danger"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card bg-light mb-3">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h5 class="card-title">Ativos: {{ $active_users }}</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                            <i class="fas fa-user-check fa-2x text-success"></i>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
